3349d1f14aaf8ce80559d4de3a658855a80bd029
Loan <loan.huynh25@student.passerellesnumeriques.org>
2024-02-21 19:46:13 +0700

    Laravel-Bai5

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,18 @@
 <h1 style="text-align: center;">Học laravel tại Unicode</h1>
+<a href="<?php echo route('admin.tintuc',['id'=>1, 'slug'=>'tin-tuc-the-gioi']);?>">Xem tin tức</a>
+
+
 <?php
 // echo date2('Y-m-d H:i:s');
 //echo env('APP_ENV');
 //echo config('app.env');
 
-if (env('APP_ENV')=='production'){
-     //Call api Live 
-     echo 'Call api Live';
-}
-else{
-//Call api sandbox 
-echo 'Call api Sandbox';
-}
+// if (env('APP_ENV')=='production'){
+//      //Call api Live 
+//      echo 'Call api Live';
+// }
+// else{
+// //Call api sandbox 
+// echo 'Call api Sandbox';
+// }
+?>
