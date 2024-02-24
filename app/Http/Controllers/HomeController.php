df80414b3643eead7e536224133590dfc1ba3b04
Loan <loan.huynh25@student.passerellesnumeriques.org>
2024-02-24 18:20:10 +0700

    Laravel-Bai8

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,12 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\View;
 
 class HomeController extends Controller
 {
     //Action index()
     public function index(){
-        return 'Home';
+        //return 'Home';
+        $title ="Hoc lap trinh web tai Unicode";
+        $content = 'Hoc lap trinh Laravel 8.x tai Unicode';
+       
+        /* 
+        [
+            'title' => $title,
+            'content' => $content
+        ]
+
+        compact('title', 'content')
+        */
+        return view('home')->with(['title'=>$title, 'content'=>$content]); //load view home.php
+
+        //return View::make('home')->with(['title'=>$title, 'content'=>$content]);
+
+        // $contentView = view('home')->render();
+        // //$contentView = $contentView->render();
+        // dd($contentView);
+        // //return $contentView;
     }
 
     //Action getNews()
@@ -18,5 +38,9 @@ class HomeController extends Controller
 
     public function getCategories($id){
         return 'Chuyên mục'.$id;
+    }
+
+    public function getProductDetail($id){
+        return view('clients.products.detail', compact('id'));
     }
 }
---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Unicode - Hoc lap trinh web</title>
+</head>
+<body>
+    <header>
+        <h1>HEADER - UNICODE</h1>
+        <h2>{{$title}}</h2>
+    </header>
+    <main>
+        <h1>Noi dung - UNICODE</h1>
+        <h2>
+            <?php echo $content; ?>
+        </h2>
+    </main>
+    <header>
+        <h1>FOOTER - UNICODE</h1>
+    </header>
+</body>
+</html>
